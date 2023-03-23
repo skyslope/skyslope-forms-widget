22530b28d4387d878ea25cac5b25c0c445ba7e78
Kyle Szombathy <kszombathy@skyslope.com>
2023-03-23 13:11:10 -0700

    styling for POC

--- src/lib/Popup.svelte
+++ src/lib/Popup.svelte
@@ -2,5 +2,5 @@
 
 </script>
 
-<iframe title="SkySlope Forms" src="https://integ-forms.skyslope.com/" width="100%" height="88vh">
+<iframe title="SkySlope Forms" src="https://integ-forms.skyslope.com/" style="width: 100%; height: 88vh;">
 </iframe>
