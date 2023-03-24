4b22b11f8214964404eefaea32d58e5b50f89403
Kyle Szombathy <kszombathy@skyslope.com>
2023-03-24 10:07:23 -0700

    hardcode to URE

--- src/lib/Popup.svelte
+++ src/lib/Popup.svelte
@@ -2,5 +2,5 @@
 
 </script>
 
-<iframe title="SkySlope Forms" src="https://integ-forms.skyslope.com/" style="width: 100%; height: 88vh;">
+<iframe title="SkySlope Forms" src="https://integ-forms.skyslope.com/sso?idp=ure" style="width: 100%; height: 88vh;">
 </iframe>
