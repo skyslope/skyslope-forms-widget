e32f968870c9893eb6e9d62ada9355e4eb0fdf19
Aaron Martin <amartin@skyslope.com>
2023-04-12 15:37:46 -0700

    WER-380: updaing svg class to hide icons if unstyled is true

--- src/components/ss-button-view-all-files/ss-button-view-all-files.tsx
+++ src/components/ss-button-view-all-files/ss-button-view-all-files.tsx
@@ -1,7 +1,7 @@
 import { Component, Host, h, Prop, Listen } from '@stencil/core';
 
-const ViewAllFilesSvg = () => (
-  <svg xmlns="http://www.w3.org/2000/svg" class="ss-icons" height="18" viewBox="0 96 960 960" width="18">
+const ViewAllFilesSvg = ({ buttonClass }: { buttonClass: string }) => (
+  <svg xmlns="http://www.w3.org/2000/svg" class={buttonClass} height="18" viewBox="0 96 960 960" width="18">
     <path d="M141 896q-24 0-42-18.5T81 836V316q0-23 18-41.5t42-18.5h280l60 60h340q23 0 41.5 18.5T881 376v460q0 23-18.5 41.5T821 896H141Z" />
   </svg>
 );
@@ -29,7 +29,7 @@ export class SsButtonViewAllFiles {
     return (
       <Host>
         <button class={this.unstyled ? '' : 'ss-button-styled'}>
-          <ViewAllFilesSvg />
+          <ViewAllFilesSvg buttonClass={this.unstyled ? 'hide-icons' : 'ss-icons'} />
           <slot></slot>
         </button>
       </Host>
